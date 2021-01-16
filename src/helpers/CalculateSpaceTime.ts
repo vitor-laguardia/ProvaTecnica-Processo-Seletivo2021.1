5de6bb570992ea3b41e47ec7229837a438d46304
vitor.laguardia <vitor.laguardia@hotmail.com>
2021-01-16 13:55:51 -0300

    Feito os calculos do Space Time

--- src/helpers/CalculateSpaceTime.ts
+++ src/helpers/CalculateSpaceTime.ts
@@ -4,10 +4,12 @@ import calculateTimeBalance from "./CalculateTimeBalance";
 
 const calculateSpaceTime = (timeMachine:TimeMachine) => {
     /**
-     * CALC
+     * CALC feito
      */
+   const SpaceTime = Math.sqrt(Math.pow(calculateTimeBalance(timeMachine).year, 2) + Math.pow(calculateTimeBalance(timeMachine).month, 2) + Math.pow(calculateTimeBalance(timeMachine).day, 2))
+  
+    return SpaceTime;
 
-    return 0;
 }
 
 export default calculateSpaceTime;
