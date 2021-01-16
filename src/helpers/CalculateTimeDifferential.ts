6e53f23db0de1e7e6c450a11f659c3d9988aae7c
vitor.laguardia <vitor.laguardia@hotmail.com>
2021-01-16 14:19:23 -0300

    Feito calculo do Time Differencial

--- src/helpers/CalculateTimeDifferential.ts
+++ src/helpers/CalculateTimeDifferential.ts
@@ -5,8 +5,11 @@ const calculateTimeDifferential = (timeMachine:TimeMachine) => {
     /**
      * CALC
      */
+    let timeBalance = calculateTimeBalance(timeMachine)
+    const TimeDifferential = timeBalance.year * 365 * 24 + timeBalance.month * 30 * 24 + timeBalance.day * 24
 
-    return 0;
+    return TimeDifferential;
+
 }
 
 export default calculateTimeDifferential;
